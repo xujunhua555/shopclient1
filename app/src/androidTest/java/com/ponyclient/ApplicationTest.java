e96e21c1beebe138842239fadb9978bdc05c4db3
xujunhua <xujunhua555@163.com>
2025-01-27 20:54:28 +0800

    xxxyyyyy

--- app/src/androidTest/java/com/ponyclient/ApplicationTest.java
+++ app/src/androidTest/java/com/ponyclient/ApplicationTest.java
@@ -9,8 +9,7 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
-
-
-
     }
+
+
 }
